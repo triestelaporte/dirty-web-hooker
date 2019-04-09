58434adba1177191e3d0df42f0840f9602155f93
triestelaporte <trieste@foglightsolutions.com>
2019-04-09 08:12:06 -0700

    Playing around

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 var express = require('express');
 var multer = require('multer');
-var request = require('request');
+//var request = require('request');
 
 var upload = multer({dest: '/tmp/'});
 var app = express();
 
 app.post('/', upload.single('thumb'), function (req, res, next) {
-    var contype = req.headers['content-type'];
-    var event = '';
-    var body = {};
+    console.log('Received Raw Webhook:  ');
+    console.log(req);
 
-    if (contype.includes('multipart/form-data')) {
-        console.log('Detected Multipart Form Data');
-        body = JSON.parse(req.body.payload);
-        event = body.event;
-    }
+    var body = JSON.parse(req.body.payload);
+    var event = body.event;
 
     console.log('Detected Event:  ' + event);
-    console.log('Got Body:  ' + body);
+    console.log('Got Body:  ');
+    console.log(body);
 
     res.json({
         message: 'ack'
